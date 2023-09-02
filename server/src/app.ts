c376ef6011da2e09224f9c72c34f63b13b591767
sarathrajan922 <sarathrajan922@gmail.com>
2023-09-02 15:52:05 +0530

    database connected successfully

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,10 @@
-
 import express from 'express';
+import connectDB from './config/Database/connection';
+import cors from 'cors'
 const app = express();
 const port = 3001
+app.use(cors())
+connectDB();
 app.listen(port,()=>{
     console.log(`server listening on ${port}`)
 })
