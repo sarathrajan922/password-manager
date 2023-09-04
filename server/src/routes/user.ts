989ef509dd8ba166ab5896cc3b12b980e33e83c9
sarathrajan922 <sarathrajan922@gmail.com>
2023-09-04 12:14:54 +0530

    delete password API done in server side

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -5,7 +5,8 @@ import userController from "../controller/userController";
 
 userRouter.get('/',userController.demo)
 userRouter.post('/add-password',userController.addPassword);
-userRouter.post('/show-password',userController.showPassword)
-userRouter.get('/get-all-passwords',userController.getAllPasswords)
+userRouter.post('/show-password',userController.showPassword);
+userRouter.get('/get-all-passwords',userController.getAllPasswords);
+userRouter.post('/delete-password',userController.deletePassword)
 
 export default userRouter;
